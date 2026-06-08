2f676e650549b6f6c40c44a3ef5ff8e3c4d6383f
SopEssential <sop.essential@gmail.com>
2026-06-08 13:46:29 +0200

    Add files via upload

---
+++ sop-pwa/sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'sop-v1';
+const ASSETS = [
+  '/sop-calculadora/',
+  '/sop-calculadora/index.html',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@400;500;700;800;900&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
